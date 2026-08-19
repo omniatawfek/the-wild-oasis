9bfb95f385c52195e4ae312ee4511e086e2a0dab
Omnia Tawfek <omniatawfek@std.mans.edu.eg>
2026-08-19 22:04:28 +0300

    add email and password to can open the application

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import FormRowVertical from "../../ui/FormRowVertical";
 import { useLogin } from "./useLogin";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("omnia@example.com");
+  const [password, setPassword] = useState("753698");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
